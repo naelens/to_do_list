0ac3282ee3afd53778d078bb52c97994c3e9a5ad
Naelem Silva <Ellenxxy19@gmail.com>
2023-03-22 14:02:18 -0300

    Finalizei area de adicionar tarefas

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,28 @@
+import { PlusCircle } from 'phosphor-react';
 import { Header } from './components/Header';
+
 import './global.css';
+
+import styles from './App.module.css'
 
 
 function App() {
   return (
-    <Header 
-      
-    />
+
+    <div>
+      <Header />
+
+      <div className={styles.addTask}>
+          <input type="text" placeholder='Adicionar uma nova tarefa'/>
+          
+          <button type="submit">Criar
+            <PlusCircle size={18}/>
+          </button>
+      </div>
+    </div>
+    
+
+ 
   )
 }
 
